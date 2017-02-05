7f5b0d297b5dcb259579b1c317140e5ff70a186f
mrvnk <marvin_knecht@gmx.de>
2017-02-05 15:47:04 +0100

    ADJ lemma added

--- src/hmm_project/morph_tags.clj
+++ src/hmm_project/morph_tags.clj
@@ -72,6 +72,46 @@
     	{:pos "ADJ"{:lemma "ver" :position "A" }{ :lemma "en" :position "E"} }
     	{:pos "ADJ"{:lemma "ver" :position "A" }{ :lemma "er" :position "E"} }
     	{:pos "ADJ"{:lemma "ver" :position "A" }{ :lemma "es" :position "E"} }
+      {{:lemma "ent" :position "A" , :lemma "er" :position "E"} :pos "ADJ"}
+	{{:lemma "ent" :position "A" , :lemma "es" :position "E"} :pos "ADJ"}
+	{{:lemma "ent" :position "A" , :lemma "e" :position "E"} :pos "ADJ"}
+	{{:lemma "ver" :position "A" , :lemma "en" :position "E"} :pos "ADJ"}
+	{{:lemma "ver" :position "A" , :lemma "er" :position "E"} :pos "ADJ"}
+	{{:lemma "ver" :position "A" , :lemma "es" :position "E"} :pos "ADJ"}
+	;;Suffixe
+	{{:lemma "bar" :position "E"} :pos "ADJ"}
+	{{:lemma "en" :position "E"} :pos "ADJ"}
+	{{:lemma "rig" :position "E"} :pos "ADJ"}
+	{{:lemma "ern" :position "E"} :pos "ADJ"}
+	{{:lemma "haft" :position "E"} :pos "ADJ"} ;;Abgrenzung zu "schaft"
+	{{:lemma "ig" :position "E"} :pos "ADJ"}
+	{{:lemma "isch" :position "E"} :pos "ADJ"}
+	{{:lemma "lich" :position "E"} :pos "ADJ"}
+	{{:lemma "los" :position "E"} :pos "ADJ"}
+	{{:lemma "mäßig" :position "E"} :pos "ADJ"}
+	{{:lemma "sam" :position "E"} :pos "ADJ"}
+	;;Kardinalzahlen
+	{{:lemma "erlei" :position "E"} :pos "ADJ"}
+	{{:lemma "fach" :position "E"} :pos "ADJ"}
+	;;Präfixe
+	{{:lemma "ab" :position "A"} :pos "ADJ"}
+	{{:lemma "aller" :position "A"} :pos "ADJ"}
+	{{:lemma "außer" :position "A"} :pos "ADJ"}
+	{{:lemma "binnen" :position "A"} :pos "ADJ"}
+	{{:lemma "erz" :position "A"} :pos "ADJ"}
+	{{:lemma "ge" :position "A"} :pos "ADJ"}
+	{{:lemma "grund" :position "A"} :pos "ADJ"}
+	{{:lemma "inner" :position "A"} :pos "ADJ"}
+	{{:lemma "miss" :position "A"} :pos "ADJ"}
+	{{:lemma "nach" :position "A"} :pos "ADJ"}
+	{{:lemma "ober" :position "A"} :pos "ADJ"}
+	{{:lemma "über" :position "A"} :pos "ADJ"}
+	{{:lemma "un" :position "A"} :pos "ADJ"}
+	{{:lemma "unter" :position "A"} :pos "ADJ"}
+	{{:lemma "ur" :position "A"} :pos "ADJ"}
+	{{:lemma "vor" :position "A"} :pos "ADJ"}
+	{{:lemma "wider" :position "A"} :pos "ADJ"}
+	{{:lemma "zwischen" :position "A"} :pos "ADJ"}
     ]
   )
 )
