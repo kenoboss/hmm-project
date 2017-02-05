f2e3dfdb8e8ee0aa2248340f11eb41214592f9cb
kenoboss <tziegelmayer@web.de>
2017-02-06 00:08:22 +0100

    update lexicon

--- src/hmm_project/get_hash_map.py
+++ src/hmm_project/get_hash_map.py
@@ -3,30 +3,40 @@ import xml.etree.ElementTree as etree
 from operator import attrgetter
 import os
 
-dictionary = {}
+# ============= AutoVivification =============
+class AutoVivification(dict):
+    def __getitem__(self, item):
+        try:
+            return dict.__getitem__(self, item)
+        except KeyError:
+            value = self[item] = type(self)()
+            return value
 
+dictionary = AutoVivification()
+
+
+# ============= reading files =============
 path = 'C:/Users/Kenobi/Ressourcen/TuebaDZ/9.0/corpora' # need the folder of TuebaDZ corpus
-#for filename in os.listdir(path):
-filename = 't_890102_141.xml' # test with one file
-tree = etree.parse(str(path)+"/"+str(filename))
-root = tree.getroot()
+for filename in os.listdir(path):
+    print(filename)
+#filename = 't_890102_141.xml' # test with one file
+    tree = etree.parse(str(path)+"/"+str(filename))
+    root = tree.getroot()
+
+    for t in root.iter('t'):
+        pos = t.attrib['pos']
+        word = t.attrib['word']
+
+        key = word
+
+# ============= create dictionary =============
+        if pos in dictionary[key]:
+            dictionary[key][pos] += 1
+        else:
+            dictionary[key][pos] = 1
 
 
-
-for t in root.iter('t'):
-    pos = t.attrib['pos']
-    word = t.attrib['word']
-
-    key = word
-
-    dictionary[key] = {}
-
-    if pos in dictionary[key]:
-        dictionary[key][pos] += 1
-    else:
-        dictionary[key][pos] = 1
-
-
+# ============= write in clj file =============
 
 target = open("output.clj", "w", encoding="utf-8")
 target.write("(def lexicon\n"
