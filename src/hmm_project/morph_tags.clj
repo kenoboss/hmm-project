d06b3e0446472e9285034e2953d3b4f61d650c4d
kenoboss <tziegelmayer@web.de>
2017-02-05 21:45:08 +0100

    some revison

--- src/hmm_project/morph_tags.clj
+++ src/hmm_project/morph_tags.clj
@@ -1,7 +1,6 @@
 ;;NN
 (def suffix_tags
-<<<<<<< HEAD
-  (set {
+  (set [
 				{:lemma "keit" :pos "NN" :position "E"}
 				{:lemma "heit" :pos "NN" :position "E"}
 				{:lemma "tum" :pos "NN" :position "E"}
@@ -24,21 +23,54 @@
 				{:lemma "gänge" :pos "NN" :position "E"}
 				{:lemma "keiten" :pos "NN" :position "E"}
 				{:lemma "nisse" :pos "NN" :position "E"}
-  })
-=======
-  (set [
-    {:lemma "keit" :pos "NN" :position "E"}
-    {:lemma "heit" :pos "NN" :position "E"}
-  	{:lemma "tum" :pos "NN" :position "E"}
-  	{:lemma "ung" :pos "NN" :position "E"}
-  	{:lemma "schaft" :pos "NN" :position "E"}
-  	{:lemma "gang" :pos "NN" :position "E"}
+
+        {:lemma "eln" :position "E" :pos "V"}
+        {:lemma "igen" :position "E" :pos "V"}
+        {:lemma "ern" :position "E" :pos "V"}
+        {:lemma "ieren" :position "E" :pos "V"}
+        {:lemma "isieren" :position "E" :pos "V"}
+
+        {:lemma "bar" :position "E" :pos "ADJ"}
+        {:lemma "en" :position "E" :pos "ADJ"}
+        {:lemma "rig" :position "E" :pos "ADJ"}
+        {:lemma "ern" :position "E" :pos "ADJ"}
+        {:lemma "haft" :position "E" :pos "ADJ"} ;;Abgrenzung zu "schaft"
+        {:lemma "ig" :position "E" :pos "ADJ"}
+        {:lemma "isch" :position "E" :pos "ADJ"}
+        {:lemma "lich" :position "E" :pos "ADJ"}
+        {:lemma "los" :position "E" :pos "ADJ"}
+        {:lemma "mäßig" :position "E" :pos "ADJ"}
+        {:lemma "sam" :position "E" :pos "ADJ"}
+        ;;Kardinalzahlen
+        {:lemma "erlei" :position "E" :pos "ADJ"}
+        {:lemma "fach" :position "E" :pos "ADJ"}
   ])
->>>>>>> origin/development
+)
+
+(def praefix_tags
+  (set [
+        {:lemma "ab" :position "A" :pos "ADJ"}
+        {:lemma "aller" :position "A" :pos "ADJ"}
+        {:lemma "außer" :position "A" :pos "ADJ"}
+        {:lemma "binnen" :position "A" :pos "ADJ"}
+        {:lemma "erz" :position "A" :pos "ADJ"}
+        {:lemma "ge" :position "A" :pos "ADJ"}
+        {:lemma "grund" :position "A" :pos "ADJ"}
+        {:lemma "inner" :position "A" :pos "ADJ"}
+        {:lemma "miss" :position "A" :pos "ADJ"}
+        {:lemma "nach" :position "A" :pos "ADJ"}
+        {:lemma "ober" :position "A" :pos "ADJ"}
+        {:lemma "über" :position "A" :pos "ADJ"}
+        {:lemma "un" :position "A" :pos "ADJ"}
+        {:lemma "unter" :position "A" :pos "ADJ"}
+        {:lemma "ur" :position "A" :pos "ADJ"}
+        {:lemma "vor" :position "A" :pos "ADJ"}
+        {:lemma "wider" :position "A" :pos "ADJ"}
+        {:lemma "zwischen" :position "A" :pos "ADJ"}
+  ])
 )
 
 
-  ;;V
 (def circumfix_tags
   (set
     [
@@ -59,59 +91,19 @@
       {:pos "V"{:lemma "über" :position "A" }{ :lemma "en" :position "E"} }
       {:pos "V"{:lemma "unter" :position "A" }{ :lemma "en" :position "E"} }
       {:pos "V"{:lemma "er" :position "A" }{ :lemma "en" :position "E"} }
-      {:lemma "eln" :position "E" :pos "V"}
-      {:lemma "igen" :position "E" :pos "V"}
-      {:lemma "ern" :position "E" :pos "V"}
-      {:lemma "ieren" :position "E" :pos "V"}
-      {:lemma "isieren" :position "E" :pos "V"}
-
-      ;; ADJ
+      
       {:pos "ADJ"{:lemma "ent" :position "A" }{ :lemma "er" :position "E"} }
     	{:pos "ADJ"{:lemma "ent" :position "A" }{ :lemma "es" :position "E"} }
     	{:pos "ADJ"{:lemma "ent" :position "A" }{ :lemma "e" :position "E"} }
     	{:pos "ADJ"{:lemma "ver" :position "A" }{ :lemma "en" :position "E"} }
     	{:pos "ADJ"{:lemma "ver" :position "A" }{ :lemma "er" :position "E"} }
     	{:pos "ADJ"{:lemma "ver" :position "A" }{ :lemma "es" :position "E"} }
-      {{:lemma "ent" :position "A" , :lemma "er" :position "E"} :pos "ADJ"}
-	{{:lemma "ent" :position "A" , :lemma "es" :position "E"} :pos "ADJ"}
-	{{:lemma "ent" :position "A" , :lemma "e" :position "E"} :pos "ADJ"}
-	{{:lemma "ver" :position "A" , :lemma "en" :position "E"} :pos "ADJ"}
-	{{:lemma "ver" :position "A" , :lemma "er" :position "E"} :pos "ADJ"}
-	{{:lemma "ver" :position "A" , :lemma "es" :position "E"} :pos "ADJ"}
-	;;Suffixe
-	{{:lemma "bar" :position "E"} :pos "ADJ"}
-	{{:lemma "en" :position "E"} :pos "ADJ"}
-	{{:lemma "rig" :position "E"} :pos "ADJ"}
-	{{:lemma "ern" :position "E"} :pos "ADJ"}
-	{{:lemma "haft" :position "E"} :pos "ADJ"} ;;Abgrenzung zu "schaft"
-	{{:lemma "ig" :position "E"} :pos "ADJ"}
-	{{:lemma "isch" :position "E"} :pos "ADJ"}
-	{{:lemma "lich" :position "E"} :pos "ADJ"}
-	{{:lemma "los" :position "E"} :pos "ADJ"}
-	{{:lemma "mäßig" :position "E"} :pos "ADJ"}
-	{{:lemma "sam" :position "E"} :pos "ADJ"}
-	;;Kardinalzahlen
-	{{:lemma "erlei" :position "E"} :pos "ADJ"}
-	{{:lemma "fach" :position "E"} :pos "ADJ"}
-	;;Präfixe
-	{{:lemma "ab" :position "A"} :pos "ADJ"}
-	{{:lemma "aller" :position "A"} :pos "ADJ"}
-	{{:lemma "außer" :position "A"} :pos "ADJ"}
-	{{:lemma "binnen" :position "A"} :pos "ADJ"}
-	{{:lemma "erz" :position "A"} :pos "ADJ"}
-	{{:lemma "ge" :position "A"} :pos "ADJ"}
-	{{:lemma "grund" :position "A"} :pos "ADJ"}
-	{{:lemma "inner" :position "A"} :pos "ADJ"}
-	{{:lemma "miss" :position "A"} :pos "ADJ"}
-	{{:lemma "nach" :position "A"} :pos "ADJ"}
-	{{:lemma "ober" :position "A"} :pos "ADJ"}
-	{{:lemma "über" :position "A"} :pos "ADJ"}
-	{{:lemma "un" :position "A"} :pos "ADJ"}
-	{{:lemma "unter" :position "A"} :pos "ADJ"}
-	{{:lemma "ur" :position "A"} :pos "ADJ"}
-	{{:lemma "vor" :position "A"} :pos "ADJ"}
-	{{:lemma "wider" :position "A"} :pos "ADJ"}
-	{{:lemma "zwischen" :position "A"} :pos "ADJ"}
+      { :pos "ADJ" {:lemma "ent" :position "A" }{ :lemma "er" :position "E"} }
+    	{:pos "ADJ"{:lemma "ent" :position "A" }{ :lemma "es" :position "E"} }
+    	{:pos "ADJ"{:lemma "ent" :position "A" }{ :lemma "e" :position "E"} }
+    	{:pos "ADJ"{:lemma "ver" :position "A" }{ :lemma "en" :position "E"} }
+    	{:pos "ADJ"{:lemma "ver" :position "A" }{ :lemma "er" :position "E"}}
+    	{:pos "ADJ"{:lemma "ver" :position "A" }{ :lemma "es" :position "E"} }
     ]
   )
 )
@@ -123,4 +115,5 @@
   [coll elm]
   (some #(= elm %) coll))
 
+;; simple test
 (in? suffix_tags {:lemma "heit" :pos "NN" :position "E"} )
