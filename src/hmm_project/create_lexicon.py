5597867f358e00fd938c619919e445d5027ed9de
kenoboss <tziegelmayer@web.de>
2017-02-07 10:42:56 +0100

    update create lexicon

--- src/hmm_project/create_lexicon.py
+++ src/hmm_project/create_lexicon.py
@@ -2,6 +2,7 @@
 import xml.etree.ElementTree as etree
 from operator import attrgetter
 import os
+import re
 
 # ============= AutoVivification =============
 class AutoVivification(dict):
@@ -16,10 +17,9 @@ dictionary = AutoVivification()
 
 
 # ============= reading files =============
-path = 'C:/Users/Kenobi/Ressourcen/TuebaDZ/9.0/corpora' # need the folder of TuebaDZ corpus
-for filename in os.listdir(path):
-    print(filename)
-#filename = 't_890102_141.xml' # test with one file
+
+def create_dictionary (filename):
+    path = 'C:/Users/Kenobi/Ressourcen/TuebaDZ/9.0/corpora' # need the folder of TuebaDZ corpus
     tree = etree.parse(str(path)+"/"+str(filename))
     root = tree.getroot()
 
@@ -29,26 +29,49 @@ for filename in os.listdir(path):
 
         key = word
 
-# ============= create dictionary =============
+    # ============= create dictionary =============
         if pos in dictionary[key]:
             dictionary[key][pos] += 1
         else:
             dictionary[key][pos] = 1
 
 
+# for filename in os.listdir(path):
+#     print(filename)
+#     create_dictionary(filename)
+filename = 't_890102_141.xml' # test with one file
+create_dictionary(filename)
+
+
+def translite (token):
+    token = re.sub("ö", "oe", token)
+    token = re.sub("ü", "ue", token)
+    token = re.sub("ä", "ae", token)
+    token = re.sub("Ö", "Oe", token)
+    token = re.sub("Ü", "Ue", token)
+    token = re.sub("Ä", "Ae", token)
+    token = re.sub("ß", "ss", token)
+
+    token = re.sub("'", "XX", token)
+
+    return token
+
+
 # ============= write in clj file =============
 
 target = open("lexicon.clj", "w", encoding="utf-8")
 target.write("(def lexicon\n"
-  "\t(set\n"
-    "\t\t[\n" )
+  "\t'(\n" )
 
 for key, value in dictionary.items():
-    target.write('\t\t\t{:word "'+str(key)+'" ')
+
+    token = translite(key)
+
+    target.write('\t { :'+str(token)+' \'(')
     for key2, value2 in dictionary[key].items():
-        target.write('{:pos "'+str(key2)+'" :counter "'+str(value2)+'" }')
+        target.write(' \'( {:pos "'+str(key2)+'" }, {:counter "'+str(value2)+'" }) ')
 
-    target.write(' }\n')
+    target.write(' ) }\n')
 
-target.write(']))')
+target.write(') )')
 target.close()
